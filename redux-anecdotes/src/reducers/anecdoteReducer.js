c5fdfcf349eed10240672d742c800e895029ea3e
Jukka Ahlgren <jukka.ahlgren@helsinki.fi>
2020-10-24 12:09:03 +0300

    6.4: anekdootit, step2
    Toteuta mahdollisuus anekdoottien äänestämiseen.
    
    	- modified:   redux-anecdotes/src/App.js
    	- modified:   redux-anecdotes/src/reducers/anecdoteReducer.js

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -24,6 +24,8 @@ const reducer = (state = initialState, action) => {
   console.log('action', action)
 
   switch (action.type) {
+    case 'NEW_ANECDOTE':
+      return [...state, action.data]
     case 'VOTE':
       const id = action.data.id
       const anecdoteToVote = state.find(n => n.id === id)
@@ -39,6 +41,16 @@ const reducer = (state = initialState, action) => {
   }
 }
 
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    data: {
+      content,
+      votes: 0,
+      id: getId()
+    }
+  }
+}
 export const voteFor = (id) => {
   return {
     type: 'VOTE',

88fabe251140510fca393a64ef5e766d953922f8
Jukka Ahlgren <jukka.ahlgren@helsinki.fi>
2020-10-24 13:13:58 +0300

    6.8: anekdootit, step6
    Eriytä anekdoottilistan näyttäminen omaksi komponentikseen nimeltään AnecdoteList.
    
    	- modified:   redux-anecdotes/src/App.js
    	- modified:   redux-anecdotes/src/components/AnecdoteForm.js
    	- copied:     redux-anecdotes/src/App.js -> redux-anecdotes/src/components/AnecdoteList.js

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,34 +1,13 @@
 import React from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
-import { voteFor } from './reducers/anecdoteReducer'
-import { useSelector, useDispatch } from 'react-redux'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    dispatch(voteFor(id))
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes
-        .sort((a, b) => b.votes - a.votes)
-        .map(b =>
-          <div key={b.id}>
-            <div>
-              {b.content}
-            </div>
-            <div>
-              has {b.votes}
-              <button onClick={() => vote(b.id)}>vote</button>
-            </div>
-          </div>
-        )}
-      <h2>create new</h2>
       <AnecdoteForm />
+      <AnecdoteList />
     </div>
   )
 }
---
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { voteFor } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch(voteFor(id))
+  }
+
+  return (
+    <ul>
+      {anecdotes
+        .sort((a, b) => b.votes - a.votes)
+        .map(b =>
+          <div key={b.id}>
+            <div>
+              {b.content}
+            </div>
+            <div>
+              has {b.votes}
+              <button onClick={() => vote(b.id)}>vote</button>
+            </div>
+          </div>
+        )}
+    </ul>
+  )
+}
+
+export default AnecdoteList
